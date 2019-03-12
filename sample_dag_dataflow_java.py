92b3d22874c695c54c365ef2aa74a5104069d468
Manjeet Bhati <46644205+msbhati08@users.noreply.github.com>
2019-03-12 14:37:01 +0530

    Update sample_dag_dataflow_java.py

--- sample_dag_dataflow_java.py
+++ sample_dag_dataflow_java.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.contrib.operators.dataflow_operator import DataFlowJavaOperator
+from airflow.operators import MyOperator
 from google.cloud import storage
 
 default_args = {
@@ -18,13 +19,14 @@ default_args = {
     }
 }
 
-dag = DAG('airflow-dataflow-invoke', description='To invoke the Dataflow',
+dag = DAG('my-dag', description='To test the airflow using various operators',
           schedule_interval='@daily',
-          start_date=datetime(2019, 3, 4), catchup=False, default_args=default_args)
+          start_date=datetime(2019, 3, 13), catchup=False, default_args=default_args)
+task1 = BashOperator(task_id="print_date", bash_command="date", dag=dag)
 
-task1 = PythonOperator(task_id='python_hello_task', python_callable=hello_func, dag=dag)
+task2 = PythonOperator(task_id='python_hello_task', python_callable=hello_func, dag=dag)
 
-task2 = DataFlowJavaOperator(
+task3 = DataFlowJavaOperator(
     task_id='dataflow_invoke_task',
     gcp_conn_id='google_cloud_default',
     jar='gs://dataflow-java-demo/jars/Main.jar',
@@ -36,4 +38,9 @@ task2 = DataFlowJavaOperator(
 
     },
     dag=dag)
+
+task4 = MyOperator(my_operator_param='This is my operator.',
+                                task_id='my_first_operator_task', dag=dag)
 task1 >> task2 
+task2 >> task3
+task3 >> task4
