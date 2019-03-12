f4b92aca6acf714b52d5308663808461a8f357a8
Manjeet Bhati <46644205+msbhati08@users.noreply.github.com>
2019-03-12 16:01:46 +0530

    Update dag1_test.py

--- src/unittest/dags/dag1_test.py
+++ src/unittest/dags/dag1_test.py
@@ -2,44 +2,65 @@ import unittest
 from airflow.models import DagBag
 
 
-class TestHelloWorldDAG(unittest.TestCase):
-    """Check HelloWorldDAG expectation"""
+class TestMySampleDAG(unittest.TestCase):
+    """Check MySampleDAG expectation"""
 
     def setUp(self):
         self.dagbag = DagBag()
 
     def test_task_count(self):
-        """Check task count of hello_world dag"""
-        dag_id = 'hello_world'
+        """Check task count of my_sample dag"""
+        dag_id = 'my_sample'
         dag = self.dagbag.get_dag(dag_id)
-        self.assertEqual(len(dag.tasks), 3)
+        self.assertEqual(len(dag.tasks), 4)
 
     def test_contain_tasks(self):
-        """Check task contains in hello_world dag"""
-        dag_id = 'hello_world'
+        """Check task contains in my_sample dag"""
+        dag_id = 'my_sample'
         dag = self.dagbag.get_dag(dag_id)
         tasks = dag.tasks
         task_ids = list(map(lambda task: task.task_id, tasks))
-        self.assertListEqual(task_ids, ['dummy_task', 'multiplyby5_task', 'hello_task'])
+        self.assertListEqual(task_ids, ['print_date_task', 'python_hello_task', 'dataflow_invoke_task', 'my_operator_task'])
 
-    def test_dependencies_of_dummy_task(self):
-        """Check the task dependencies of dummy_task in hello_world dag"""
-        dag_id = 'hello_world'
+    def test_dependencies_of_print_date_task(self):
+        """Check the task dependencies of print_date_task in my_sample dag"""
+        dag_id = 'my_sample'
         dag = self.dagbag.get_dag(dag_id)
-        dummy_task = dag.get_task('dummy_task')
+        print_date_task = dag.get_task('print_date_task')
 
         upstream_task_ids = list(map(lambda task: task.task_id, dummy_task.upstream_list))
         self.assertListEqual(upstream_task_ids, [])
         downstream_task_ids = list(map(lambda task: task.task_id, dummy_task.downstream_list))
-        self.assertListEqual(downstream_task_ids, ['hello_task', 'multiplyby5_task'])
+        self.assertListEqual(downstream_task_ids, ['python_hello_task', 'dataflow_invoke_task', 'my_operator_task'])
 
-    def test_dependencies_of_hello_task(self):
-        """Check the task dependencies of hello_task in hello_world dag"""
-        dag_id = 'hello_world'
+    def test_dependencies_of_python_hello_task(self):
+        """Check the task dependencies of python_hello_task in my_sample dag"""
+        dag_id = 'my_sample'
         dag = self.dagbag.get_dag(dag_id)
-        hello_task = dag.get_task('hello_task')
+        python_hello_task = dag.get_task('python_hello_task')
 
         upstream_task_ids = list(map(lambda task: task.task_id, hello_task.upstream_list))
-        self.assertListEqual(upstream_task_ids, ['dummy_task'])
+        self.assertListEqual(upstream_task_ids, ['print_date_task'])
+        downstream_task_ids = list(map(lambda task: task.task_id, hello_task.downstream_list))
+        self.assertListEqual(downstream_task_ids, ['dataflow_invoke_task', 'my_operator_task'])
+
+    def test_dependencies_of_python_hello_task(self):
+        """Check the task dependencies of dataflow_invoke_task in my_sample dag"""
+        dag_id = 'my_sample'
+        dag = self.dagbag.get_dag(dag_id)
+        dataflow_invoke_task = dag.get_task('dataflow_invoke_task')
+
+        upstream_task_ids = list(map(lambda task: task.task_id, hello_task.upstream_list))
+        self.assertListEqual(upstream_task_ids, ['print_date_task', 'python_hello_task'])
+        downstream_task_ids = list(map(lambda task: task.task_id, hello_task.downstream_list))
+        self.assertListEqual(downstream_task_ids, ['my_operator_task'])
+     def test_dependencies_of_python_hello_task(self):
+        """Check the task dependencies of my_operator_task in my_sample dag"""
+        dag_id = 'my_sample'
+        dag = self.dagbag.get_dag(dag_id)
+        my_operator_task = dag.get_task('my_operator_task')
+
+        upstream_task_ids = list(map(lambda task: task.task_id, hello_task.upstream_list))
+        self.assertListEqual(upstream_task_ids, ['print_date_task', 'python_hello_task', 'dataflow_invoke_task'])
         downstream_task_ids = list(map(lambda task: task.task_id, hello_task.downstream_list))
         self.assertListEqual(downstream_task_ids, [])
