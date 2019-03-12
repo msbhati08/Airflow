48c448100b4950c416a79665984cfce2e98ce055
Manjeet Bhati <46644205+msbhati08@users.noreply.github.com>
2019-03-12 14:17:06 +0530

    Create operator1.py

---
+++ plugins/operator1.py
@@ -0,0 +1,23 @@
+import logging
+
+from airflow.models import BaseOperator
+from airflow.plugins_manager import AirflowPlugin
+from airflow.utils.decorators import apply_defaults
+
+log = logging.getLogger(__name__)
+
+class MyOperator(BaseOperator):
+
+    @apply_defaults
+    def __init__(self, my_operator_param, *args, **kwargs):
+        self.operator_param = my_operator_param
+        super(MyOperator, self).__init__(*args, **kwargs)
+
+    def execute(self, context):
+        log.info("Hello World!")
+        log.info('operator_param: %s', self.operator_param)
+        return (self.operator_param * 2)
+
+class MyPlugin(AirflowPlugin):
+    name = "my_plugin"
+    operators = [MyOperator]
